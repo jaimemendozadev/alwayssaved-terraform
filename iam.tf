a2cba2fb58f7ef7ebbaeb3fa2c084ee9866998fb
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-09 08:14:10 -0700

    Move SQS IAM policy to iam.tf file

--- iam.tf
+++ iam.tf
@@ -47,6 +47,41 @@ resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
 }
 
 
+# Add permissions to allow the EC2 instance to send & receive messages
+resource "aws_iam_policy" "sqs_access_policy" {
+  name        = "NotecastsSQSAccessPolicy"
+  description = "Allows EC2 instances to send & receive SQS messages"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sqs:SendMessage",
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ],
+      "Resource": [
+        "${aws_sqs_queue.extractor_push_queue.arn}",
+        "${aws_sqs_queue.extractor_pull_queue.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_sqs_policy" {
+  role       = aws_iam_role.notecasts_ec2_role.name
+  policy_arn = aws_iam_policy.sqs_access_policy.arn
+}
+
+
+
 # Creates an Instance Profile, which is required for EC2 instances to use IAM roles.
 # Links the IAM Role `notecasts-ec2-role` to the profile.
 resource "aws_iam_instance_profile" "notecasts_instance_profile" {
--- sqs.tf
+++ sqs.tf
@@ -13,36 +13,4 @@ resource "aws_sqs_queue" "extractor_pull_queue" {
   fifo_queue                 = false
 }
 
-# Add permissions to allow the EC2 instance to send & receive messages
-resource "aws_iam_policy" "sqs_access_policy" {
-  name        = "NotecastsSQSAccessPolicy"
-  description = "Allows EC2 instances to send & receive SQS messages"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "sqs:SendMessage",
-        "sqs:ReceiveMessage",
-        "sqs:DeleteMessage",
-        "sqs:GetQueueAttributes"
-      ],
-      "Resource": [
-        "${aws_sqs_queue.extractor_push_queue.arn}",
-        "${aws_sqs_queue.extractor_pull_queue.arn}"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-# Attach the SQS Policy to the EC2 IAM Role
-resource "aws_iam_role_policy_attachment" "attach_sqs_policy" {
-  role       = aws_iam_role.notecasts_ec2_role.name
-  policy_arn = aws_iam_policy.sqs_access_policy.arn
-}
 
