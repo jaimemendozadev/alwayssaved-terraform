9480009ec725628b1691d8af9426dea41aad0540
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-08 16:51:19 -0800

    Add notecasts vpc, subnet, & security group

---
+++ vpc.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "notecasts_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "notecasts-vpc"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.notecasts_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "notecasts-public-subnet"
+  }
+}
+
+resource "aws_security_group" "notecasts_sg" {
+  vpc_id = aws_vpc.notecasts_vpc.id
+
+  # Allow inbound HTTP/HTTPS for API
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "notecasts-sg"
+  }
+}
