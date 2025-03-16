05ba1bb2cb6030f02b7759f7f73ed6aca7e35fb4
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-15 17:08:13 -0700

    Consolidate ec2 IAM policies

--- iam.tf
+++ iam.tf
@@ -1,27 +1,6 @@
-# Principal = "ec2.amazonaws.com" -> Allows EC2 instances to use this role.
-# sts:AssumeRole -> Lets EC2 instances temporarily "borrow" the role’s permissions.
-resource "aws_iam_role" "notecasts_ec2_role" {
-  name = "notecasts-ec2-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "notecasts_s3_access" {
-  name        = "NotecastsS3AccessPolicy"
-  description = "Allow EC2 instances to read/write from S3"
+resource "aws_iam_policy" "notecasts_ec2_policy" {
+  name        = "NotecastsEC2Policy"
+  description = "Allows EC2 instances to access S3, SQS, and Parameter Store"
 
   policy = <<EOF
 {
@@ -34,28 +13,7 @@ resource "aws_iam_policy" "notecasts_s3_access" {
         "s3:GetObject"
       ],
       "Resource": "arn:aws:s3:::notecasts/*"
-    }
-  ]
-}
-EOF
-}
-
-# Attaches the S3 policy to the IAM role so the EC2 instance can access the S3 bucket.
-resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-  role       = aws_iam_role.notecasts_ec2_role.name
-  policy_arn = aws_iam_policy.notecasts_s3_access.arn
-}
-
-
-# Add permissions to allow the EC2 instance to send & receive messages
-resource "aws_iam_policy" "sqs_access_policy" {
-  name        = "NotecastsSQSAccessPolicy"
-  description = "Allows EC2 instances to send & receive SQS messages"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+    },
     {
       "Effect": "Allow",
       "Action": [
@@ -68,27 +26,7 @@ resource "aws_iam_policy" "sqs_access_policy" {
         "${aws_sqs_queue.extractor_push_queue.arn}",
         "${aws_sqs_queue.extractor_pull_queue.arn}"
       ]
-    }
-  ]
-}
-EOF
-}
-
-
-resource "aws_iam_role_policy_attachment" "attach_sqs_policy" {
-  role       = aws_iam_role.notecasts_ec2_role.name
-  policy_arn = aws_iam_policy.sqs_access_policy.arn
-}
-
-
-resource "aws_iam_policy" "ssm_access_policy" {
-  name        = "NotecastsSSMAccessPolicy"
-  description = "Allows EC2 instances to read SecureString parameters"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+    },
     {
       "Effect": "Allow",
       "Action": [
@@ -96,25 +34,14 @@ resource "aws_iam_policy" "ssm_access_policy" {
         "ssm:GetParameters",
         "ssm:GetParametersByPath"
       ],
-      "Resource": [
-        "arn:aws:ssm:us-east-1:YOUR_ACCOUNT_ID:parameter/notecasts/*"
-      ]
+      "Resource": "arn:aws:ssm:us-east-1:YOUR_ACCOUNT_ID:parameter/notecasts/*"
     }
   ]
 }
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "attach_ssm_policy" {
+resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
   role       = aws_iam_role.notecasts_ec2_role.name
-  policy_arn = aws_iam_policy.ssm_access_policy.arn
-}
-
-
-
-# Creates an Instance Profile, which is required for EC2 instances to use IAM roles.
-# Links the IAM Role `notecasts-ec2-role` to the profile.
-resource "aws_iam_instance_profile" "notecasts_instance_profile" {
-  name = "notecasts-instance-profile"
-  role = aws_iam_role.notecasts_ec2_role.name
+  policy_arn = aws_iam_policy.notecasts_ec2_policy.arn
 }
