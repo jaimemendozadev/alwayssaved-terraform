e7cc0d4896a9972503e6fc4d379b16571158227b
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-04 16:50:10 -0700

    Add embedding service ec2 aws_instance block
    Still need to fix it.

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,28 @@ resource "aws_instance" "audio_extractor" {
   }
 }
 
+# TODO: Embedding ec2 Instance will need its own security group
+resource "aws_instance" "embedding_service" {
+  ami                         = var.embedding_ami_id
+  instance_type               = var.embedding_instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  security_groups             = [aws_security_group.always_saved_sg.id]
+  associate_public_ip_address = false
+  iam_instance_profile        = aws_iam_instance_profile.always_saved_instance_profile.name
+  key_name                    = var.aws_pub_key_name
 
+  user_data = templatefile("${path.module}/scripts/embedding_service_setup.sh", {
+    ECR_URL = var.aws_ecr_embedding_service_url
+  })
 
+  root_block_device {
+    volume_size = 100 # 🔥 Increase to 100 GB for plenty of breathing room
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "always-saved-embedding-service"
+  }
+}
 
 
