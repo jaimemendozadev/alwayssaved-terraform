9cc68b751816f5663bdf7412a8f70cc33e0f1705
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-04 18:00:05 -0700

    Update iam.tf with embedding instance role

--- iam.tf
+++ iam.tf
@@ -1,5 +1,13 @@
+#################################################
+# Extractor Service
+#################################################
+
+
 # Principal = "ec2.amazonaws.com" -> Allows EC2 instances to use this role.
 # sts:AssumeRole -> Lets EC2 instances temporarily "borrow" the role’s permissions.
+
+
+# Original ec2 Role for Extractor Service
 resource "aws_iam_role" "always_saved_ec2_role" {
   name = "always-saved-ec2-role"
 
@@ -19,6 +27,7 @@ resource "aws_iam_role" "always_saved_ec2_role" {
 EOF
 }
 
+
 resource "aws_iam_role_policy_attachment" "attach_ssm_managed_policy" {
   role       = aws_iam_role.always_saved_ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
@@ -29,6 +38,8 @@ resource "aws_iam_role_policy_attachment" "attach_cloudwatch_agent_policy" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+
+# Original ec2 IAM Policy for Extractor Service
 # TODO: Need to create a new /alwayssaved s3 Bucket
 resource "aws_iam_policy" "always_saved_ec2_policy" {
   name        = "AlwaysSavedEC2Policy"
@@ -91,6 +102,7 @@ resource "aws_iam_policy" "always_saved_ec2_policy" {
 EOF
 }
 
+# Original IAM Instance Profile for Extractor Service
 resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
   role       = aws_iam_role.always_saved_ec2_role.name
   policy_arn = aws_iam_policy.always_saved_ec2_policy.arn
@@ -101,5 +113,123 @@ resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
 resource "aws_iam_instance_profile" "always_saved_instance_profile" {
   name = "always-saved-instance-profile"
   role = aws_iam_role.always_saved_ec2_role.name
+}
+
+
+
+#################################################
+# Embedding Service
+#################################################
+
+
+
+# ec2 Role for Embedding Service 
+resource "aws_iam_role" "always_saved_embedding_ec2_role" {
+  name = "always-saved-embedding-ec2-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ssm_managed_policy_to_embedding" {
+  role       = aws_iam_role.always_saved_embedding_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "attach_cloudwatch_agent_policy_to_embedding" {
+  role       = aws_iam_role.always_saved_embedding_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+
+# ec2 IAM Policy for Embedding Service
+# TODO: Need to create a new /alwayssaved s3 Bucket
+resource "aws_iam_policy" "always_saved_embedding_ec2_policy" {
+  name        = "AlwaysSavedEmbeddingEC2Policy"
+  description = "Allows Embedding EC2 instances to access S3, SQS, and Parameter Store"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::notecasts/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Resource": "arn:aws:s3:::${var.aws_s3_code_bucket_name}/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sqs:SendMessage",
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ],
+      "Resource": [
+        "${aws_sqs_queue.extractor_push_queue.arn}",
+        "${aws_sqs_queue.embedding_push_queue.arn}"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameter",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+
+
+
+
+
+# IAM Instance Profile for Embedding Service
+resource "aws_iam_role_policy_attachment" "attach_embedding_ec2_policy" {
+  role       = aws_iam_role.always_saved_embedding_ec2_role.name
+  policy_arn = aws_iam_policy.always_saved_embedding_ec2_policy.arn
+}
+
+resource "aws_iam_instance_profile" "always_saved_embedding_instance_profile" {
+  name = "always-saved-embedding-instance-profile"
+  role = aws_iam_role.always_saved_embedding_ec2_role.name
 }
 
