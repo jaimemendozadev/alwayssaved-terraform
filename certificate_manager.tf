0862a0085c2e0e77ebf6e43704ae2ca06095008c
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-25 07:39:41 -0700

    Add certificate manager code

---
+++ certificate_manager.tf
@@ -0,0 +1,42 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "alwayssaved.com"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "www.alwayssaved.com"
+  ]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "alwayssaved-cert"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 300
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
