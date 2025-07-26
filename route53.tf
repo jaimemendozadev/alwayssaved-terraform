b4ca044be8aa7f987c88fd974fcf8008ed320327
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-25 17:43:12 -0700

    Add A Record for www.alwayssaved.com

--- route53.tf
+++ route53.tf
@@ -13,3 +13,19 @@ resource "aws_route53_record" "app_alias" {
   #   prevent_destroy = true
   # }
 }
+
+resource "aws_route53_record" "app_alias_www" {
+  zone_id = var.route53_zone_id
+  name    = "www.alwayssaved.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.alwayssaved_alb.dns_name
+    zone_id                = aws_lb.alwayssaved_alb.zone_id
+    evaluate_target_health = true
+  }
+
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
